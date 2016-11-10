ece7e25d65f789adb6696e99609f7984558781e6
Tin Wong <wongt1@wit.edu>
2016-11-10 10:51:31 -0500

    Added methods into Player.java
    Rough sketch of some methods for Player, Hand should be a Stack so I'll
    change methods according to that

---
+++ src/edu/wit/comp2000/group23/lists/Tests/TestPlayer.java
@@ -0,0 +1,14 @@
+package edu.wit.comp2000.group23.lists.Tests;
+
+import org.junit.Test;
+
+public class TestPlayer {
+	@Test
+	/**
+	 * to do later (hand needs to be implemented)
+	 */
+	public void testPlayer(){
+		//Hand hand = new Hand();
+		//Player player = new Player(0, hand);
+	}
+}
