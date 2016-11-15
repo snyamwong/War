523595f526470382b7cb05fe00ffa1740a7671ba
connerj1 <connerj1@wit.edu>
2016-11-15 15:31:59 -0500

    Constructor
    Added Constructor

--- src/edu/wit/comp2000/group23/lists/Utils/Table.java
+++ src/edu/wit/comp2000/group23/lists/Utils/Table.java
@@ -5,4 +5,13 @@ package edu.wit.comp2000.group23.lists.Utils;
 public class Table {
 	private Player[] players;
 	
+	
+	public Table(){
+		
+	}
+	
+	public Table(int numOfPlayers){
+		players = new Player[numOfPlayers];
+	}
+	
 }
